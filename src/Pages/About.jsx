5af47c8153ad7050b3f92c337a0a56668160eb57
Aidan Tang <aidanroom124@gmail.com>
2024-06-07 22:58:57 -0400

    Updated About page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -4,7 +4,7 @@ export default function About(){
             <h1 className="mt-4 font-['abuget'] text-[9rem] m-auto text-center leading-[1.1]">The Story Of Urban Artisans</h1>
             <p className="tracking-[0.4rem] m-auto text-center text-xl mb-6">OUR HISTORY, WHAT WE ARE, AND THE FUTURE</p>
             <div className="h-1 max-w-[18rem] bg-white m-auto mb-10"/>
-            <p className="max-w-[70%] text-center m-auto">The idea of Urban Artisans came from a simple obervation. We noticed how soulless the modern world felt at times. We lived in a world where everyone was divided by cars and the same standard corporate look everywhere you go.</p>
+            <p className="max-w-[70%] text-center m-auto">The idea of Urban Artisans came from a simple obervation. We noticed how soulless the modern world felt at times. We lived in a world where everyone was divided by cars and the same standard corporate look everywhere you go. But even amongst all the corporate drab that lies in our cities, we believe that creativity, even if suppressed, still exists in our communities today, and as such, we had an idea. We wanted to create a community that supports creatives and a better life for all, contrary to common business. But of course, we needed a main purpose, and what better purpose than to fuel our creative communities with the world's most delicious coffee? Hence, Urban Artisans was born. Whether you create, or just want some of the finest coffee modernity has to offer, our community will always welcome you, no matter where you go.</p>
         </>
     )
 }
