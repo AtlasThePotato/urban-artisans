dda112ce081dcfb7b2a857317b8f0758bab9a947
Aidan Tang <aidanroom124@gmail.com>
2024-06-10 14:43:52 -0400

    Added more to the Invest Page (06/10/2024)

--- src/Pages/Invest.jsx
+++ src/Pages/Invest.jsx
@@ -4,7 +4,21 @@
 export default function Invest(){
     return(
         <>
-            <h1>This is the Investor's page!</h1>
+            <h1 className="mt-4 font-['abuget'] text-[9rem] m-auto text-center leading-[1.1]">For Investors & Potential Partners</h1>
+            <p className="tracking-[0.4rem] m-auto text-center text-xl mb-6">WHY YOU SHOULD INVEST IN URBAN ARTISANS</p>
+            <div className="h-1 max-w-[18rem] bg-white m-auto mb-10"/>
+            <p className="max-w-[70%] text-center m-auto">Currently, we are not accepting any new partners within our business, but being a strong believer in constant change, we can never be too sure of the future. As such, we would like to provide you with why we are the most optimal coffee brand.</p>
+
+            <div className="flex border-4 border-solid border-white m-12">
+                <div className="w-[70%] p-2">
+                    <h1 className="font-['hey-november'] text-[5rem]">Our Environmental Practices</h1>
+                
+                </div>
+
+                <div className="w-[30%] bg-gray-500 bg-[url('/src/assets/invest-environment-photo.jpeg')] bg-cover border-l-4 border-white border-solid bg-center">
+                </div>
+
+            </div>
         </>
     )
 }
