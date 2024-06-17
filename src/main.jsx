f44d8d3a4ab0d87c0b7396d770accc872cc061b5
Aidan Tang <aidanroom124@gmail.com>
2024-06-17 10:16:05 -0400

    Another Set Of Corrections

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/urban-artisans">
+    <BrowserRouter basename="/urban-artisans/">
       <App/>
     </BrowserRouter>
   </React.StrictMode>
